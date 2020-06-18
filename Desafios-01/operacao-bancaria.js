a794435635c6eb054a2f7f9c541aa7a0bbc4acc5
Lucas Pedroso <lucassppedroso@gmail.com>
2020-06-18 11:34:55 -0300

    commit contendo a finalização do desafio-04

--- Desafios-01/operacao-bancaria.js
+++ Desafios-01/operacao-bancaria.js
@@ -1,13 +1,91 @@
 const user = {
     name: "Lucas",
-    transaction: [],
+    transactions: [],
     balance: 0,
 };
 
-function createTransaction(transaction) {
-    user.transaction.push(transaction);
+// Criar novas transações e atualizar o atributo 'balance'.
 
-    // if(){
-        
-    // }
+function createTransaction(transaction) {
+    user.transactions.push(transaction);
+
+    if(transaction.type === "credit") {
+        user.balance += transaction.value;
+
+    } else {
+        user.balance -= transaction.value;
+
+    }
 }
+
+// Retorna as transações de maior valor credit/debit.
+
+function getHigherTransactionByType(type) {
+
+    let higherTransaction;
+    let higherValue = 0;
+
+    for(let transaction of user.transactions) {
+
+        if(transaction.type == type && transaction.value > higherValue) {
+            higherValue = transaction.value;
+            higherTransaction = transaction;
+        }
+    }
+
+    return higherTransaction;
+}
+
+// Média de transações realizadas.
+
+function getAverageTransactionValue() {
+
+    let sum = 0;
+
+    for(let transaction of user.transactions) {
+        sum += transaction.value;
+
+    }
+
+    return sum / user.transactions.length;
+}
+
+// Retorna um objeto contendo o total de transações credit/debit
+
+function getTransactionsCount() {
+
+    let creditCont = 0;
+    let debitCont = 0;
+
+    for(let transaction of user.transactions) {
+
+        if(transaction.type == "credit") {
+            creditCont += 1;
+        }
+
+        if(transaction.type == "debit") {
+            debitCont += 1;
+        }
+
+    }
+
+    return(user.occurrence = {credit: creditCont, debit: debitCont});
+}
+
+
+createTransaction({type: "credit", value: 50});
+createTransaction({type: "credit", value: 100});
+createTransaction({type: "credit", value: 50});
+createTransaction({type: "debit", value: 50});
+createTransaction({type: "debit", value: 60});
+
+
+
+console.log(getHigherTransactionByType("credit"));
+console.log(getHigherTransactionByType("debit"));
+
+console.log(getAverageTransactionValue());
+
+console.log(getTransactionsCount());
+
+console.log(user);
