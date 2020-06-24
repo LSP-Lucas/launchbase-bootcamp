8c891923da4955b3483aa0572b63a0e05be4364c
Lucas Pedroso <lucassppedroso@gmail.com>
2020-06-24 16:08:43 -0300

    criação do servidor back-end e inserção da template engine

--- Modulo-03/server.js
+++ Modulo-03/server.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
+
 const server = express();
 
+server.use(express.static('public'));
+
+server.set("view engine", "njk");
+
+nunjucks.configure("views", {
+    express: server
+})
+
 server.get("/", function(req, res) {
-    return res.send("Olá!!!!!");
+    return res.render("about");
+});
+
+server.get("/portfolio", function(req, res) {
+    return res.render("portfolio");
 });
 
 server.listen(5000, function() {
