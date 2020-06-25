329c059dd9eacc73c8c1f8b722720f9b220f7d06
Lucas Pedroso <lucassppedroso@gmail.com>
2020-06-25 16:18:58 -0300

    criação da base de dados dinâmica

---
+++ Modulo-03/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "GykTLqODQuU",
+        title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
+        duration: "30 min",
+        price: "Free"
+    },
+    {
+        id: "GykTLqODQuU",
+        title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
+        duration: "15 min",
+        price: "Free"
+    },
+    {
+        id: "GykTLqODQuU",
+        title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
+        duration: "11 min",
+        price: "Free"
+    },
+    {
+        id: "GykTLqODQuU",
+        title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
+        duration: "57 min",
+        price: "Free"
+    },
+    {
+        id: "GykTLqODQuU",
+        title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
+        duration: "22 min",
+        price: "30,00"
+    },
+    {
+        id: "GykTLqODQuU",
+        title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
+        duration: "28 min",
+        price: "59,90"
+    },
+];
--- Modulo-03/server.js
+++ Modulo-03/server.js
@@ -2,21 +2,35 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 
 const server = express();
+const videos = require("./data");
 
 server.use(express.static('public'));
 
 server.set("view engine", "njk");
 
 nunjucks.configure("views", {
-    express: server
-})
+    express: server,
+    autoescape: false
+});
 
 server.get("/", function(req, res) {
-    return res.render("about");
+    const about = {
+        avatar_url: "https://avatars3.githubusercontent.com/u/11745960?s=460&u=41ee2ea5a18ab45724d8b87d63acdea2893d8c82&v=4",
+        name: "Lucas Pedroso",
+        role: "Estudante Full Stack",
+        description: 'Cursando o Bootcamp LaunchBase da <a href="https://rocketseat.com.br" target="_blank">Rocketseat</a>',
+        links: [
+            { name: "Github", url: "https://github.com/LSP-Lucas" },
+            { name: "Linkdin", url: "https://www.linkedin.com/in/lucas-da-silva-pedroso-0b4420191/" }
+        ]
+    };
+
+    return res.render("about", { about });
 });
 
 server.get("/portfolio", function(req, res) {
-    return res.render("portfolio");
+
+    return res.render("portfolio", { items: videos });
 });
 
 server.listen(5000, function() {
