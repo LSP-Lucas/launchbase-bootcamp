509c2694029f1e840bb4da67809c730fde67c9f8
Lucas Pedroso <lucassppedroso@gmail.com>
2020-06-26 11:11:29 -0300

    atualização dos Ids dos cards

--- Modulo-03/data.js
+++ Modulo-03/data.js
@@ -6,33 +6,33 @@ module.exports = [
         price: "Free"
     },
     {
-        id: "GykTLqODQuU",
-        title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
-        duration: "15 min",
+        id: "-j7vLmBMsEU",
+        title: "Deploy de aplicação NodeJS e ReactJS no Heroku | Diego Fernandes",
+        duration: "24 min",
         price: "Free"
     },
     {
-        id: "GykTLqODQuU",
-        title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
-        duration: "11 min",
+        id: "2G_mWfG0DZE",
+        title: "Testes no NodeJS aplicando TDD com Jest | Diego Fernandes",
+        duration: "1h20 min",
         price: "Free"
     },
     {
-        id: "GykTLqODQuU",
-        title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
-        duration: "57 min",
+        id: "HN1UjzRSdBk",
+        title: "Desvendando o CSS Grid na prática | Mayk Brito",
+        duration: "36 min",
         price: "Free"
     },
     {
-        id: "GykTLqODQuU",
-        title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
-        duration: "22 min",
+        id: "6WB16wZS61c",
+        title: "React Hooks na prática | Diego Fernandes",
+        duration: "32 min",
         price: "30,00"
     },
     {
-        id: "GykTLqODQuU",
-        title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
-        duration: "28 min",
+        id: "u99tNt3TZf8",
+        title: "Desvendando o Redux na prática | Diego Fernandes",
+        duration: "50 min",
         price: "59,90"
     },
 ];
